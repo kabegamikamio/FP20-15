d60d610aea74c68906792075381e6154eacfb106
izrn-yukikaze <nmktkm1112@outlook.jp>
2021-01-27 22:11:42 +0900

    コメントアウトを追加しました

--- sample.c
+++ sample.c
@@ -6,7 +6,7 @@
 #include <math.h>
 #include "img.h"
 
-void img_fillrect(struct color c, double x, double y, double w, double h) {
+void img_fillrect(struct color c, double x, double y, double w, double h) { //長方形
     int imin = (int)(x - w/2), imax = (int)(x + w/2);
     int jmin = (int)(y - h/2), jmax = (int)(y + h/2);
     int i, j;
@@ -15,7 +15,20 @@ void img_fillrect(struct color c, double x, double y, double w, double h) {
     }
 }
 
-void img_fillellipse(struct color c, double x, double y, double rx, double ry) {
+void img_fillcircle(struct color c, double x, double y, double r){ //円
+    int imin = (int)(x-r-1), imax = (int)(x+r+1);
+    int jmin = (int)(y-r-1), jmax = (int)(y+r+1);
+    int i, j;
+    for(j = jmin; j <= jmax; j++){
+        for(i = imin; i <= imax; i++){
+            if((x-i)*(x-i) + (y-j)*(y-j) <= r*r){
+                img_putpixel(c, i, j);
+            }
+        }
+    }
+}
+
+void img_fillellipse(struct color c, double x, double y, double rx, double ry) { //楕円
     int imin = (int)(x - rx - 1), imax = (int)(x + rx + 1);
     int jmin = (int)(y - ry - 1), jmax = (int)(y + ry + 1);
     int i, j;
@@ -29,7 +42,7 @@ void img_fillellipse(struct color c, double x, double y, double rx, double ry) {
 }
 
 void img_fillrotellipse(struct color c, double x, double y, double rx, double ry,
-                        double theta) {
+                        double theta) { //回転した楕円
     int d = (rx > ry) ? rx : ry;
     int imin = (int)(x - d - 1), imax = (int)(x + d + 1);
     int jmin = (int)(y - d - 1), jmax = (int)(y + d + 1);
@@ -82,7 +95,7 @@ void img_fillconvex(struct color c, int n, double ax[], double ay[]) {
 }
 
 void img_filltriangle(struct color c, double x0, double y0,
-                      double x1, double y1, double x2, double y2) {
+                      double x1, double y1, double x2, double y2) { //三角形
     double ax1[] = { x0, x1, x2, x0 }, ax2[] = { x0, x2, x1, x0 };
     double ay1[] = { y0, y1, y2, y0 }, ay2[] = { y0, y2, y1, y0 };
     img_fillconvex(c, 3, ax1, ay1);
@@ -90,7 +103,7 @@ void img_filltriangle(struct color c, double x0, double y0,
 }
 
 void img_fillline(struct color c, double x0, double y0,
-                  double x1, double y1, double w) {
+                  double x1, double y1, double w) { //直線
     double dx = y1-y0, dy = x0-x1, n = 0.5*w / sqrt(dx*dx + dy*dy);
     dx *= n; dy *= n;
     double ax[] = { x0-dx, x0+dx, x1+dx, x1-dx, x0-dx };
