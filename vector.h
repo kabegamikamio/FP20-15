1e3c20beff60601eece308b4d8e0f83b805046df
USAtype-C <77767644+USAtype-C@users.noreply.github.com>
2021-02-08 12:44:10 +0900

    Update vector.h

--- vector.h
+++ vector.h
@@ -7,3 +7,4 @@ double dot(struct vector a, struct vector b);                       //内積
 struct vector product(struct vector a, struct vector b);            //外積
 struct vector normlize(struct vector a);                            //正規化
 struct vector rotation(struct vector a, double theta, double phi);  //回転
+struct vector reflect(struct vector a, struct vector p, struct vector q);//反射、ベクトルpqからなる平面
