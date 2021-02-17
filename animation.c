7f629e4f6089853c8f1d30269dfafef46b71cb7f
USAtype-C <77767644+USAtype-C@users.noreply.github.com>
2021-02-17 16:47:51 +0900

    Update animation.c

--- animation.c
+++ animation.c
@@ -5,15 +5,15 @@
 struct vector S = {100,0,50};
 struct vector G = {0,0,100};
 
-struct vector cutin(struct vector S, struct vector G,int length, int frame){
+struct vector decmov(struct vector S, struct vector G,int length, int frame){
     struct vector way;
         way.x = S.x - G.x,
         way.y = S.y - G.y,
         way.z = S.z - G.z;
         double prog = pow(2,-5 * frame % length);
-            struct vector cin;
-                cin.x = S.x + (prog * way.x),
-                cin.y = S.y + (prog * way.y),
-                cin.z = S.z + (prog * way.z);
-            return cin;
+            struct vector dec;
+                dec.x = S.x + (prog * way.x),
+                dec.y = S.y + (prog * way.y),
+                dec.z = S.z + (prog * way.z);
+            return dec;
 }
