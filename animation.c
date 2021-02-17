50b56143cc4bafb79e7daa860ef532856ed7ce6e
USAtype-C <77767644+USAtype-C@users.noreply.github.com>
2021-02-17 20:45:37 +0900

    Update animation.c

--- animation.c
+++ animation.c
@@ -5,7 +5,7 @@
 struct vector S = {100,0,50};
 struct vector G = {0,0,100};
 
-struct vector decmov(struct vector S, struct vector G, float length, int frame){
+struct vector decmov(struct vector S, struct vector G,float length, int frame){
     struct vector way;
         way.x = S.x - G.x,
         way.y = S.y - G.y,
@@ -19,27 +19,31 @@ struct vector decmov(struct vector S, struct vector G, float length, int frame){
 }
 
 struct vector kurukuru(struct vector S, double theta ,double phi, float length, int frame){
+    double Theta = theta * M_PI/180;
+    double Phi = phi * M_PI/180;
     struct vector xy;
-        xy.x =   S.x * cos(theta*frame/length) + S.y * sin(theta*frame/length);
-        xy.y = - S.x * sin(theta*frame/length) + S.y * cos(theta*frame/length);
+        xy.x =   S.x * cos(Theta*frame/length) + S.y * sin(Theta*frame/length);
+        xy.y = - S.x * sin(Theta*frame/length) + S.y * cos(Theta*frame/length);
         xy.z = S.z;
-        struct vector kur;
-        kur.x = xy.x * cos(phi*frame/length) - xy.z * sin(phi*frame/length);
+    struct vector kur;
+        kur.x = xy.x * cos(Phi*frame/length) - xy.z * sin(Phi*frame/length);
         kur.y = xy.y;
-        kur.z = xy.x * sin(phi*frame/length) + xy.z * cos(phi*frame/length);
+        kur.z = xy.x * sin(Phi*frame/length) + xy.z * cos(Phi*frame/length);
     return kur;
 }
 
 
 struct vector deckuru(struct vector S, double theta ,double phi, float length, int frame){
+    double Theta = theta * M_PI/180;
+    double Phi = phi * M_PI/180;
     double ratio = pow(2,-5*frame/length) - pow(2,-5)*frame/length;
     struct vector xy;
-        xy.x =   S.x * cos(theta*ratio*frame/length) + S.y * sin(theta*ratio*frame/length);
-        xy.y = - S.x * sin(theta*ratio*frame/length) + S.y * cos(theta*ratio*frame/length);
+        xy.x =   S.x * cos(Theta*ratio*frame/length) + S.y * sin(Theta*ratio*frame/length);
+        xy.y = - S.x * sin(Theta*ratio*frame/length) + S.y * cos(Theta*ratio*frame/length);
         xy.z = S.z;
         struct vector deck;
-        deck.x = xy.x * cos(phi*ratio*frame/length) - xy.z * sin(phi*ratio*frame/length);
+        deck.x = xy.x * cos(Phi*ratio*frame/length) - xy.z * sin(Phi*ratio*frame/length);
         deck.y = xy.y;
-        deck.z = xy.x * sin(phi*ratio*frame/length) + xy.z * cos(phi*ratio*frame/length);
+        deck.z = xy.x * sin(Phi*ratio*frame/length) + xy.z * cos(Phi*ratio*frame/length);
     return deck;
 }
