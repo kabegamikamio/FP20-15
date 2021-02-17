c0674df9a6b952ae3de9e5f9c7b338d670fe368c
USAtype-C <77767644+USAtype-C@users.noreply.github.com>
2021-02-17 21:45:36 +0900

    Update animation.c

--- animation.c
+++ animation.c
@@ -31,18 +31,26 @@ struct vector decmov(struct vector S, struct vector G, float length, int frame){
             return decm;
 }
 
-struct vector kurukuru(struct vector S, double theta ,double phi, float length, int frame){
+struct vector kurukuru(struct vector S, double theta ,double phi, double a, double b, double c, float length, int frame){
     double Theta = theta * M_PI/180;
     double Phi = phi * M_PI/180;
+    struct vector S1;
+        S1.x = S.x -a,
+        S1.y = S.y -b,
+        S1.z = S.z -c;
     struct vector xy;
-        xy.x =   S.x * cos(Theta*frame/length) + S.y * sin(Theta*frame/length);
-        xy.y = - S.x * sin(Theta*frame/length) + S.y * cos(Theta*frame/length);
-        xy.z = S.z;
-    struct vector kur;
-        kur.x = xy.x * cos(Phi*frame/length) - xy.z * sin(Phi*frame/length);
-        kur.y = xy.y;
-        kur.z = xy.x * sin(Phi*frame/length) + xy.z * cos(Phi*frame/length);
-    return kur;
+        xy.x =   S1.x * cos(Theta*frame/length) + S1.y * sin(Theta*frame/length);
+        xy.y = - S1.x * sin(Theta*frame/length) + S1.y * cos(Theta*frame/length);
+        xy.z = S1.z;
+    struct vector xz;
+        xz.x = xy.x * cos(Phi*frame/length) - xy.z * sin(Phi*frame/length);
+        xz.y = xy.y;
+        xz.z = xy.x * sin(Phi*frame/length) + xy.z * cos(Phi*frame/length);
+    struct vector Kur;
+        Kur.x = xz.x + a,
+        Kur.y = xz.y + b,
+        Kur.z = xz.z + c;
+    return Kur;
 }
 
 
